ff8f9c084b983f8cf79914493e3d0e5b524d46a6
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-21 21:02:39 -0500

    set up some security measures and additional logging.  starting to build mongoDB database for BE

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000/',   //React FE local server
+    'https://www.URL.com',      //URL to be determined
+    'https://URL.com'           //TBD URL w/o www.
+]
+
+module.exports = allowedOrigins
