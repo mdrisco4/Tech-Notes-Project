bc49bec10dca2b5cdcfc5f3d8a3180a18ac8f170
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-21 20:49:57 -0500

    set up cors.  setting up access to the API

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require ('cookie-parser')
+const cors = require('cors')
 const PORT = process.env.PORT || 3500
 
 app.use(logger)
 
+app.use(cors())
+
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
@@ -22,5 +29,7 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
