3bb765449a9bc6e289e82673d634d425c2e23cea
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-11-23 12:43:35 -0500

    created controller and routes for notes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,17 +2,25 @@ const User = require('../models/User')
 const Note = require('../models/Note')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
+// const mongoose = require('mongoose');
 
 // @desc Get all users
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find().select('-password').lean()
-    if (!users) {
+    if (!users?.length) {
         return res.status(400).json({ message: 'No users found' })
     }
     res.json(users)
 })
+
+
+// {
+// 	"username": "Hank",
+// 	"password": "1234abcd",
+// 	"roles": ["Employee"]
+// }
 
 // @desc Create new user
 // @route POST /users
@@ -21,7 +29,10 @@ const createNewUser = asyncHandler(async (req, res) => {
     const { username, password, roles } = req.body
 
     //Confirm data
-    if(!username || !password || !Array.isArray(roles) || roles.length) {
+    if(!username || !password || !Array.isArray(roles) || !roles.length) {
+        console.log(username)
+        console.log(password)
+        console.log(roles)
         return res.status(400).json({ message: 'All fields are required' })
     }
 
@@ -54,7 +65,7 @@ const updateUser = asyncHandler(async (req, res) => {
     const { id, username, roles, active, password } = req.body
 
     //Confirm data
-    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !=='boolean') {
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
         return res.status(400).json({ message: 'All fields are required' })
     }
     
@@ -80,7 +91,7 @@ const updateUser = asyncHandler(async (req, res) => {
         user.password = await bcrypt.hash(password, 10) // salt rounds
     }
 
-    const updateUser = await user.save()
+    const updatedUser = await user.save()
 
     res.json({ message: `${updatedUser.username} updated` })
 })
@@ -95,8 +106,8 @@ const deleteUser = asyncHandler(async (req, res) => {
         return res.status(400).json({ message: 'User ID Required' })
     }
 
-    const notes = await Note.findOne({ user: id }).lean().exec()
-    if (notes?.length) {
+    const note = await Note.findOne({ user: id }).lean().exec()
+    if (note) {
         return res.status(400).json({ message: 'User has assigned notes' })
     }
 
@@ -107,7 +118,7 @@ const deleteUser = asyncHandler(async (req, res) => {
 
     }
 
-    const result = await user.deleteOne()
+    const result = await user.deleteOne({ user: id })
 
     const reply = `Username ${result.username} with ID ${result._id} deleted`
 
