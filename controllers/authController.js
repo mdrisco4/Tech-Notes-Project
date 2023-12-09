1f21272f9881a870b9740f0c9490583eaef8c438
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-09 16:13:38 -0500

    created ACCESS TOKEN and REFRESH TOKEN for env file and started working on authorization logic.  taking break and fincishing later

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,21 @@ const asyncHandler = require('express-async-handler')
 // @route POST /auth
 // @access Public
 const login = asyncHandler(async (req, res) => {
-    //actions
+    const { username, password } = req.body
+
+    if (!username || !password) {
+        return res.status(400).json({ message: 'All fields are required' })
+
+        const foundUser = await User.findOne({ username }).exec()
+
+        if (!foundUser || !foundUser.active) {
+            return res.status(401).json({ message: 'Unauthorized' })
+        }
+
+        const match = await bcrypt.compare(password, foundUser.password)
+
+        if(!match) return res.status(401).json({ message: 'Unauthorized' })
+    }
 })
 
 // @desc Refresh
@@ -19,4 +33,13 @@ const refresh = (req, res) => {
 
 // @desc Logout
 // @route POST /auth/logout
-// @access Public
+// @access Public - to clear sookie if needed
+const logout = (req, res) => {
+
+}
+
+module.exports = {
+    login,
+    refresh,
+    logout
+}
