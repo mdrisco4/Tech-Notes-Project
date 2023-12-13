43d9ec13d0dbf65d4243ce9e1b5220cf7c58002b
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-12 21:01:12 -0500

    set up token refresh function

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,7 +56,36 @@ const login = asyncHandler(async (req, res) => {
 // @route GET /auth/refresh
 // @access Public - when access token expires
 const refresh = (req, res) => {
+    const cookies = req.cookies
 
+    if (!cookies?.jwt) return res.status(401).json({ message: 'Unauthorized' })
+
+    const refreshToken = cookies.jwt
+
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        asyncHandler(async (err, decoded) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' })
+
+            const foundUser = await User.findOne({ username: jwt.decoded.username }).exec()
+
+            if (!foundUser) return res.status(401).json({ message: 'Unauthorized' })
+
+            const accessToken = jwt.sign(
+                {
+                    "UserInfo": {
+                        "username": foundUser.username,
+                        "roles": foundUser.roles
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '15m' }
+            )
+
+            rex.json({ accessToken })
+        })
+    )
 }
 
 // @desc Logout
