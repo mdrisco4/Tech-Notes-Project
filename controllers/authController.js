7b1b43538ea590a3ccd1cbf6b76e1408e6d4ff1c
michaeldriscoll <michaelndriscoll81@gmail.com>
2023-12-14 14:04:56 -0500

    set up middleware for JWT verification

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,7 +95,7 @@ const logout = (req, res) => {
     const cookies = req.cookies
     if (!cookies?.jwt) return res.sendStatus(204) // No content
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
-    res.json({ message: 'COokie cleared' })
+    res.json({ message: 'Cookie cleared' })
 
 }
 
